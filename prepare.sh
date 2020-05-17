32c502f82f2c7cd0fd497f78278049dad5ef9c0b
Ângelo Gonçalves Dias <agdias@gmail.com>
2020-05-17 19:07:42 -0300

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -6,3 +6,10 @@ sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+
+#
+
+deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo apt update
+sudo apt install ansible
